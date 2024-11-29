6afe4739f1d6d15876ae35aec87b65b7cac6f9d3
edrzapi <edrzapi@gmail.com>
2024-11-29 00:10:18 +0000

    Initial Commit

---
+++ IOS-API-Tutorial/IOS_API_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_API_TutorialApp.swift
+//  IOS-API-Tutorial
+//
+//  Created by Ed on 29/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct IOS_API_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS-API-TutorialTests/IOS_API_TutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_API_TutorialTests.swift
+//  IOS-API-TutorialTests
+//
+//  Created by Ed on 29/11/2024.
+//
+
+import Testing
+@testable import IOS_API_Tutorial
+
+struct IOS_API_TutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
